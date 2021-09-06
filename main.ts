d45fc4c8f07f5cfac7b77d94362e05aab7586772
Chrischoy58 <76098330+Chrischoy58@users.noreply.github.com>
2021-09-06 09:49:27 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.showString("Hello!")
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+})
